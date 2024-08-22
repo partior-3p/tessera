2ff0a1301dc453af67966efa32337a28eaf61a28
john-sobrepena-partior <john.sobrepena@partior.com>
2024-08-22 18:45:29 +0800

    SET-559 config validations

--- key-vault/hashicorp-key-vault/src/main/java/com/quorum/tessera/key/vault/hashicorp/HashicorpDbCredentialsVaultServiceFactory.java
+++ key-vault/hashicorp-key-vault/src/main/java/com/quorum/tessera/key/vault/hashicorp/HashicorpDbCredentialsVaultServiceFactory.java
@@ -1,10 +1,12 @@
 package com.quorum.tessera.key.vault.hashicorp;
 
 import com.quorum.tessera.config.Config;
+import com.quorum.tessera.config.KeyVaultConfig;
 import com.quorum.tessera.config.KeyVaultType;
 import com.quorum.tessera.config.util.EnvironmentVariableProvider;
 import com.quorum.tessera.key.vault.DbCredentialsVaultServiceFactory;
 
+import java.util.List;
 import java.util.Objects;
 
 public class HashicorpDbCredentialsVaultServiceFactory extends HashicorpVaultServiceFactory implements DbCredentialsVaultServiceFactory {
@@ -26,12 +28,27 @@ public class HashicorpDbCredentialsVaultServiceFactory extends HashicorpVaultSer
 
     return super.create(config, envProvider, util,
       (appConfig)->{
-        return appConfig.getJdbcConfig()
+        var keyVaultConfig = appConfig.getJdbcConfig()
           .getHashicorpVaultDbCredentialsConfig()
           .toKeyVaultConfig();
+        validateRequiredConfigurationPropertiesArePresent(keyVaultConfig);
+        return keyVaultConfig;
       },
       HashicorpDbCredentialsVaultService::new
     );
+  }
+
+  void validateRequiredConfigurationPropertiesArePresent(KeyVaultConfig keyVaultConfig){
+    var requiredProperties = List.of("url","dbSecretEngineName","vaultDbRole","approlePath");
+    var missingProperties = requiredProperties.stream().filter(
+      propName->
+        !keyVaultConfig.hasProperty(propName) || keyVaultConfig.getProperty(propName).isEmpty()
+    ).toList();
+    if (!missingProperties.isEmpty()){
+      throw new HashicorpVaultException(
+        String.format("[%s] missing in the configuration. This/these properties should be defined in configuration section: jdbc.hashicorpVaultDbCredentialsConfig", String.join(", ", missingProperties))
+      );
+    }
   }
 
   public KeyVaultType getType() {
