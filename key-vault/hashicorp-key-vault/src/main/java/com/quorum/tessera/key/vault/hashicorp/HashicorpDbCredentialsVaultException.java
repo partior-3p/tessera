b486f37dcb40748dc3adad6baea43c843e2b1dc4
john-sobrepena-partior <john.sobrepena@partior.com>
2024-08-23 17:49:51 +0800

    SET-559 included hashicorp specific exception type

--- key-vault/hashicorp-key-vault/src/main/java/com/quorum/tessera/key/vault/hashicorp/HashicorpDbCredentialsVaultException.java
+++ key-vault/hashicorp-key-vault/src/main/java/com/quorum/tessera/key/vault/hashicorp/HashicorpDbCredentialsVaultException.java
@@ -1,9 +1,6 @@
 package com.quorum.tessera.key.vault.hashicorp;
 
 public class HashicorpDbCredentialsVaultException extends RuntimeException {
-  HashicorpDbCredentialsVaultException(Throwable cause) {
-    super(cause);
-  }
 
   HashicorpDbCredentialsVaultException(String message) {
     super(message);
