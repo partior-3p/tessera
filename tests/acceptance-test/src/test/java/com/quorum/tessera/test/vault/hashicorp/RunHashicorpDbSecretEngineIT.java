bc86006988ecadf5853e37dcef9c7b142075a921
john-sobrepena-partior <john.sobrepena@partior.com>
2024-08-24 17:09:42 +0800

    SET-559 added postgresql server for UAT IT

---
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/vault/hashicorp/RunHashicorpDbSecretEngineIT.java
@@ -0,0 +1,12 @@
+package com.quorum.tessera.test.vault.hashicorp;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+  features = "build/resources/test/features/vault/hashicorp-db-secret-engine.feature",
+  plugin = {"pretty"})
+public class RunHashicorpDbSecretEngineIT {
+}
