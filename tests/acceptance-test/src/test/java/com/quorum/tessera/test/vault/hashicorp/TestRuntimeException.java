0fc4788463f8d9a7e3c1359277007464dcd76b76
john-sobrepena-partior <john.sobrepena@partior.com>
2024-08-26 10:25:06 +0800

    SET-559 spotless java changes

--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/vault/hashicorp/TestRuntimeException.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/vault/hashicorp/TestRuntimeException.java
@@ -1,15 +1,15 @@
 package com.quorum.tessera.test.vault.hashicorp;
 
-public class TestRuntimeException extends RuntimeException{
-  public TestRuntimeException(Throwable throwable){
+public class TestRuntimeException extends RuntimeException {
+  public TestRuntimeException(Throwable throwable) {
     super(throwable);
   }
 
-  public TestRuntimeException(String message){
+  public TestRuntimeException(String message) {
     super(message);
   }
 
-  public TestRuntimeException(String message, Throwable throwable){
+  public TestRuntimeException(String message, Throwable throwable) {
     super(message, throwable);
   }
 }
