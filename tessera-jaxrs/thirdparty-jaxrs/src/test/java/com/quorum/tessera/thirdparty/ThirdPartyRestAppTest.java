a6ccc9025af63c7e0785afa56bcf03ab0049bdbf
matthew-alexander-partior <matthew.alexander@partior.com>
2024-11-08 03:25:10 +0000

    test: SET-574 update singletons count

--- tessera-jaxrs/thirdparty-jaxrs/src/test/java/com/quorum/tessera/thirdparty/ThirdPartyRestAppTest.java
+++ tessera-jaxrs/thirdparty-jaxrs/src/test/java/com/quorum/tessera/thirdparty/ThirdPartyRestAppTest.java
@@ -42,7 +42,7 @@ public class ThirdPartyRestAppTest {
 
     Set<Object> results = thirdParty.getSingletons();
 
-    assertThat(results).hasSize(4);
+    assertThat(results).hasSize(5);
     List<Class> types = results.stream().map(Object::getClass).collect(Collectors.toList());
     assertThat(types)
         .containsExactlyInAnyOrder(
